ea62c77f4579effbdb5bf13b46865dfd3ed442e1
pouyasadeqii <52070678+pouyasadeqii@users.noreply.github.com>
2023-01-09 19:27:54 +0330

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
+import menu from "../assets/images/textalign-left.svg";
+import close from "../assets/images/close.svg";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
     <section className="bg-[#FDEBEA] w-full h-[815px] pt-8">
-      <header className="container max-w-[1180px] mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-2">
+      <header className=" px-6 md:px-32 bg-[#FDEBEA] rounded-lg sticky top-1  mx-auto flex justify-between items-center">
+        <div className="hidden lg:flex  items-center gap-2">
           <img src={logo} alt="logo" />
           <span className="text-accent font-bold">e-learning</span>
         </div>
 
-        <nav>
+        {/* mobile menu */}
+
+        <div className="lg:hidden cursor-pointer">
+          <img
+            src={menu}
+            alt="menu"
+            onClick={() => setMobileMenu(!mobileMenu)}
+          />
+        </div>
+
+        {/* end mobile menu */}
+
+        <nav className="hidden lg:block">
           <ul className="flex gap-8 font-semibold text-myblack">
             <li>
-              <a href="#" className="text-accent" >Home</a>
+              <a href="#" className="text-accent">
+                Home
+              </a>
             </li>
             <li>
               <a href="#">Courses</a>
@@ -30,11 +48,58 @@ const Header = () => {
           </ul>
         </nav>
 
-        <div className="flex items-center gap-7">
-          <a href="#" className="text-accent font-semibold">Login</a>
-          <a href="#" className="rounded-lg bg-accent text-secondary px-4 py-3">Sign Up For Free</a>
+        <div className="flex items-center gap-3 lg:gap-7">
+          <a
+            href="#"
+            className="text-accent text-sm lg:text-base lg:font-semibold"
+          >
+            Login
+          </a>
+          <a
+            href="#"
+            className="rounded-lg bg-accent text-secondary text-sm lg:text-base p-2 lg:px-4 lg:py-3"
+          >
+            Sign Up For Free
+          </a>
         </div>
       </header>
+      {mobileMenu && (
+        <div className="bg-primary mobilemenu w-10/12 sm:w-1/2 md:w-1/2 p-3 fixed top-0 h-full overflow-y-scroll transition-all duration-300">
+          <div className="flex items-center justify-between border-b-2 border-accent py-2">
+            <div className="flex items-center gap-4">
+              <img src={logo} alt="logo" />
+              <span className="text-accent font-bold">e-learning</span>
+            </div>
+            <img
+              className="cursor-pointer"
+              src={close}
+              alt="close"
+              onClick={() => setMobileMenu(!mobileMenu)}
+            />
+          </div>
+          <nav className="mt-8">
+            <ul className="flex flex-col gap-5 font-semibold text-myblack">
+              <li>
+                <a href="#" className="text-accent">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#">Courses</a>
+              </li>
+              <li>
+                <a href="#">Articles</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </section>
   );
 };
