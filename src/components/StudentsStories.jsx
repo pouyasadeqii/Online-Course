8121bc1c2c8ab1dc3da5ef8a24598d7fbe3cad7e
pouyasadeqii <52070678+pouyasadeqii@users.noreply.github.com>
2023-02-18 02:09:03 +0330

    FEAT : add some changes and complete student stories component

--- src/components/StudentsStories.jsx
+++ src/components/StudentsStories.jsx
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import contentWritting from "../assets/images/contentWritting.jpg";
+import poster from "../assets/images/7.jpg";
+import four from "../assets/images/1.jpg";
+import two from "../assets/images/2.jpg";
+import three from "../assets/images/3.jpg";
+import five from "../assets/images/4.jpg";
+import six from "../assets/images/5.jpg";
+import one from "../assets/images/6.jpg";
+import play from "../assets/images/play.svg";
 import sapmle from "../assets/videos/sample.mp4";
 import graphicStudent from "../assets/videos/graphicStudent.mp4";
 import uiStudent from "../assets/videos/uiStudent.mp4";
@@ -17,11 +25,31 @@ const videos = {
   webStudent3: webStudent3,
 };
 
+const images = {
+  uiStudent: one,
+  pythonStudent: four,
+  graphicStudent: five,
+  webStudent1: two,
+  webStudent2: three,
+  webStudent3: six,
+};
+
 const StudentsStories = () => {
   const [video, setVideo] = useState(sapmle);
+  const [videoPoster, setVideoPoster] = useState(poster);
+  const [flag, setFlag] = useState(true);
+
+  const videoRef = useRef();
 
   const videoHandler = (e) => {
     setVideo(videos[e.target.alt]);
+    setVideoPoster(images[e.target.alt]);
+    setFlag(true);
+  };
+
+  const showHandler = () => {
+    setFlag(false);
+    videoRef.current.play();
   };
 
   return (
@@ -37,13 +65,21 @@ const StudentsStories = () => {
           {/* 1 */}
           <div className="flex flex-col gap-4 w-full">
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="uiStudent"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={one}
+                  alt="uiStudent"
+                  className="wfull h-full object-cover rounded cursor-pointer z-50"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="uiStudent"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -53,13 +89,21 @@ const StudentsStories = () => {
             </div>
 
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="webStudent1"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={two}
+                  alt="webStudent1"
+                  className="w-20 h-20 object-cover rounded cursor-pointer"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="webStudent1"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -71,13 +115,21 @@ const StudentsStories = () => {
             </div>
 
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="webStudent2"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={three}
+                  alt="webStudent2"
+                  className="w-20 h-20 object-cover rounded cursor-pointer"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="webStudent2"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -91,13 +143,21 @@ const StudentsStories = () => {
           {/* 2 */}
           <div className="flex flex-col gap-4 w-full">
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="pythonStudent"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={four}
+                  alt="pythonStudent"
+                  className="w-20 h-20 object-cover rounded cursor-pointer"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="pythonStudent"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -107,13 +167,21 @@ const StudentsStories = () => {
             </div>
 
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="graphicStudent"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={five}
+                  alt="graphicStudent"
+                  className="w-20 h-20 object-cover rounded cursor-pointer"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="graphicStudent"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -125,13 +193,21 @@ const StudentsStories = () => {
             </div>
 
             <div className="w-full bg-white p-3 rounded-lg flex items-center gap-3">
-              <img
-                loading="lazy"
-                src={contentWritting}
-                alt="webStudent3"
-                className="w-20 h-20 object-cover rounded cursor-pointer"
-                onClick={videoHandler}
-              />
+              <div className="w-20 h-20 relative">
+                <img
+                  loading="lazy"
+                  src={six}
+                  alt="webStudent3"
+                  className="w-20 h-20 object-cover rounded cursor-pointer"
+                  onClick={videoHandler}
+                />
+                <img
+                  src={play}
+                  alt="webStudent3"
+                  className="absolute top-7 left-7 cursor-pointer"
+                  onClick={videoHandler}
+                />
+              </div>
               <div className="flex flex-col gap-4">
                 <span className="font-semibold text-myblack text-sm">
                   John Esmith
@@ -145,12 +221,23 @@ const StudentsStories = () => {
         </div>
 
         {/* video */}
-        <div className="bg-blue-100 w-full rounded-lg overflow-hidden">
+        <div className="bg-blue-100 w-full rounded-lg overflow-hidden relative">
           <video
+            ref={videoRef}
             src={video}
             className="w-full h-full object-cover"
             controls
+            poster={videoPoster}
+            onClick={() => setFlag(false)}
           ></video>
+          {flag && (
+            <img
+              src={play}
+              alt="play"
+              className="absolute w-14 h-14 top-[30%] sm:top-[40%] sm:left-[47%] left-[44%] cursor-pointer"
+              onClick={showHandler}
+            />
+          )}
         </div>
       </div>
     </div>
